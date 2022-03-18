deab1320cfdbf6dc19f4bb12b6b10545c08f1ab5
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2022-03-18 16:29:27 -0700

    Latest

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,4 +1,6 @@
 # Guess The Number Experiment - Empirical Results
+from functools import partial
+from random import randint
 
 def _check_attempt(attempt, answer):
     """Check the attempt.
@@ -16,3 +18,12 @@ def _check_attempt(attempt, answer):
         When both are False attempt == answer
     """
     return (answer > attempt, answer < attempt)
+
+def new_game():
+    """Create a new game.
+    
+    Returns
+    -------
+    callable(): A _check_attempt() with answer defined.
+    """
+    return partial(_check_attempt, answer=randint(1, 100))
