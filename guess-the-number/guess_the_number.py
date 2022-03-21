2e1e41d36f970167f2b3d1c4fd9fbff88da550d7
Shapedsundew9 <16618209+Shapedsundew9@users.noreply.github.com>
2022-03-21 15:59:42 -0400

    Got animated guess the number chart working.

--- guess-the-number/guess_the_number.py
+++ guess-the-number/guess_the_number.py
@@ -1,18 +1,28 @@
 # Guess The Number Experiment - Empirical Results
 from functools import partial
 from random import randint
-from tqdm import trange
+from tqdm import trange, tqdm
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, PillowWriter
+from numpy import array, digitize, bincount, arange
+from numpy.random import shuffle
+
 
 # Constants
 LOW = 1
 HIGH = 100
 MAX_ATTEMPTS = 1000
 NUM_GAMES = 1000
+NUM_BARS = 100
+BAR_STEP = int(MAX_ATTEMPTS / NUM_BARS)
+HALF_BAR_STEP = int(0.5 * BAR_STEP)
+BATCH_SIZE = 10
+NUM_FRAMES = int(NUM_GAMES / BATCH_SIZE) + 1
 
 
 def _check_attempt(attempt, answer):
     """Check the attempt.
-    
+
     Args
     ----
     attempt (int): The attempt
@@ -30,7 +40,7 @@ def _check_attempt(attempt, answer):
 
 def new_game():
     """Create a new game.
-    
+
     Returns
     -------
     callable(): A _check_attempt() with answer defined.
@@ -39,8 +49,8 @@ def new_game():
 
 
 def constant_strategy():
-    """The random constant strategy.
-    
+    """Random constant strategy.
+
     Randomly choose a constant in the range LOW to HIGH.
 
     Returns
@@ -55,8 +65,8 @@ def constant_strategy():
 
 
 def random_strategy():
-    """The random strategy.
-    
+    """Random strategy.
+
     Randomly choose an integer in the range LOW to HIGH inclusive.
 
     Returns
@@ -69,9 +79,28 @@ def random_strategy():
     return func
 
 
+def random_memory_strategy():
+    """Random memory strategy.
+
+    Randomly choose an integer in the range LOW to HIGH inclusive
+    but do not repeat a choice.
+
+    Returns
+    -------
+    callable(): Strategy function returning a random integer in the range
+        LOW to HIGH inclusive with no replacement.
+    """
+    valid = arange(LOW, HIGH + 1)
+    shuffle(valid)
+    valid = list(valid)
+    def func(_, __):
+        return valid.pop()
+    return func
+
+
 def play_game(strategy):
     """Play a game using the suplied strategy.
-    
+
     Args
     ----
     strategy(callable): Callable takes no arguments and returns an integer.
@@ -93,7 +122,7 @@ def play_game(strategy):
 
 def experiment_one():
     """Execute experiment 1.
-    
+
     Games are played by executing strategies. A strategy (functions with names
     *_strategy taking no parameters) defines a strategy function which is used to
     play a game. The strategy function takes two boolean parameters: higher &
@@ -102,12 +131,33 @@ def experiment_one():
     the new_game() function which returns higher & lower for each guess.
     """
     results = {}
-    for strategy in filter(lambda x: '_strategy' in x, dir()):
+    for strategy in filter(lambda x: '_strategy' in x, globals()):
         name = ' '.join(strategy.split('_')[:-1])
-        func = globals[strategy]
-        results[name] = [play_game(strategy) for _ in trange(LOW, HIGH + 1, desc=f'Strategy {name}')]
+        func = globals()[strategy]
+        results[name] = array([play_game(func) for _ in trange(0, MAX_ATTEMPTS, desc=f'Strategy {name}')])
 
-    
+
+    x = array(range(0, MAX_ATTEMPTS, BAR_STEP)) + HALF_BAR_STEP
+    fig = plt.figure(figsize=(16, 6))
+    ax = fig.add_axes([.05,.1,.9,.8])
+    plt.xlim([0, MAX_ATTEMPTS])
+    plt.ylim([0, NUM_GAMES])
+    series = {name: ax.bar([], [], width=BAR_STEP, alpha=0.5) for name in results}
+    pbar = tqdm(total=NUM_FRAMES, desc='Building GIF')
+
+    def animation(frame):
+        batch = BATCH_SIZE * frame
+        pbar.update(1)
+        ax.clear()
+        plt.xlim([0, MAX_ATTEMPTS])
+        plt.ylim([0, NUM_GAMES])
+        for name, bar in series.items():
+            y = bincount(digitize(results[name][0:batch], range(10, NUM_BARS * BAR_STEP, BAR_STEP)), minlength=NUM_BARS)
+            ax.bar(x, y, width=BAR_STEP, alpha=0.5)
+
+    gif = FuncAnimation(fig, animation, frames=100)
+    gif.save('video.gif', dpi=100, writer=PillowWriter(fps=25))
+
 
 if __name__ == "__main__":
     experiment_one()
